fd9bb7c494d542ddad2b1d16cd6549b78f8679e0
uriya <ooria47@gmail.com>
2022-06-02 18:41:52 +0300

    make it better

--- src/util/function.ts
+++ src/util/function.ts
@@ -16,7 +16,6 @@ export function checkInput(values: string[]) {
 }
 
 export function createGraphqlClient(token?: string) {
-	const endpoint = process.env.DB_URL;
-	console.log(endpoint);
+	const endpoint = process.env.DB_URL!;
 	return new GraphQLClient(endpoint, {headers: {Authorization: "Bearer " + token}});
 }
